03570626041144c96beed61fa67d0334e5737018
kabramovich <wieker@gmail.com>
2020-01-29 00:48:07 +0200

    Started serial port

--- mqtt-client/src/test/java/org/fusesource/mqtt/client/CheckConnectionTest.java
+++ mqtt-client/src/test/java/org/fusesource/mqtt/client/CheckConnectionTest.java
@@ -1,14 +1,39 @@
 package org.fusesource.mqtt.client;
 
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+
 public class CheckConnectionTest {
     public static void main(String[] args) throws Exception {
         MQTT mqtt = new MQTT();
         mqtt.setHost("localhost", 1883);
 
-        BlockingConnection connection = mqtt.blockingConnection();
+        final BlockingConnection connection = mqtt.blockingConnection();
         connection.connect();
 
-        connection.publish("foo", "Hello".getBytes(), QoS.AT_LEAST_ONCE, false);
+        SerialPort commPort = SerialPort.getCommPort("/dev/ttyACM1");
+        System.out.println(commPort.openPort());
+        if (!commPort.isOpen()) {
+            System.out.println("fuckup");
+            return;
+        }
+
+        commPort.addDataListener(new SerialPortDataListener() {
+            public int getListeningEvents() {
+                return 0;
+            }
+
+            public void serialEvent(SerialPortEvent serialPortEvent) {
+                System.out.println(serialPortEvent.getReceivedData());
+                try {
+                    connection.publish("foo", "Hello".getBytes(), QoS.AT_LEAST_ONCE, false);
+                } catch (Exception e) {
+                    System.out.println("ec " + e);
+                }
+            }
+        });
+
 
         Topic[] topics = {new Topic("foo", QoS.AT_LEAST_ONCE)};
         byte[] qoses = connection.subscribe(topics);
